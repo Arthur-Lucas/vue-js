5f3799b9394e1754632a9e99ce8a2acab2471254
Arthur <arthur.lucas@edu.devinci.fr>
2022-03-16 18:54:37 +0100

    :bug: previous content are now deleted

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
-    <div :style="db">
+    <div class="carousel-form">
       <p>Lorem ipsum dolor sit amet. Sed voluptates perspiciatis non cumque incidunt qui minus quia qui repellendus voluptatibus sed iusto totam sit rerum aspernatur sit voluptas quaerat. A incidunt nisi ea illo tempore At nihil sint. Et exercitationem earum At saepe animi ab quos nobis. Et eius quae sed quis quia ut culpa voluptatem sit minima nesciunt in porro quas et exercitationem voluptas.
 
 Et earum eius a nisi quas ut tenetur tempora. Ut atque expedita et laboriosam corporis vel adipisci pariatur ad esse dolorem et omnis culpa sed dolorem iusto vel pariatur atque. A quam eaque a assumenda velit eum repellendus nisi ab veniam exercitationem est architecto dolores. Et quae totam non aperiam soluta 33 quidem soluta.
 
 A expedita veritatis id omnis natus non voluptates commodi nam autem molestiae qui harum sint qui adipisci maxime hic nemo facilis. Ea quia deserunt quo quia debitis ad labore nesciunt.</p>
     </div>
-    <div :style="db">
+    <div class="carousel-form">
         <form @submit.prevent="display">
             <button type="submit">Testez vos connaissances</button>
         </form>
@@ -22,16 +22,17 @@ A expedita veritatis id omnis natus non voluptates commodi nam autem molestiae q
 export default {
    data(){
      return{
-       db: 'display:block',
        dn: 'display:none'
      }
    },
    methods:{
      display: function(){
-         
-       this.db = 'display:none';
-       this.dn = 'display:block';
-       console.log(this.db);
+        let array = document.querySelectorAll('.carousel-form')
+        console.log(array)
+        array.forEach(element => {
+            element.parentNode.removeChild(element)
+        });
+        this.dn = 'display:block';
      }
    }
 }
