98b0f99047748140fd8c3692ac0ec3cc24c26f05
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-17 11:26:07 +0100

    :memo: Question Test

--- src/components/question-component.vue
+++ src/components/question-component.vue
@@ -1,7 +1,65 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const questions = [
+  {
+    id: "0",
+    questionText: "This is a Question,1",
+    Answers: [
+      {
+        isValid: true,
+        AnswerText: "asnswer 1",
+      },
+      {
+        isValid: false,
+        AnswerText: "asnswer 2",
+      },
+      {
+        isValid: false,
+        AnswerText: "asnswer 3",
+      },
+      {
+        isValid: false,
+        AnswerText: "asnswer 4",
+      },
+    ],
+  },
+  {
+    id: "1",
+    questionText: "This is a Question 2",
+    Answers: [
+      {
+        isValid: false,
+        AnswerText: "asnswer 1",
+      },
+      {
+        isValid: false,
+        AnswerText: "asnswer 2",
+      },
+      {
+        isValid: true,
+        AnswerText: "asnswer 3",
+      },
+      {
+        isValid: false,
+        AnswerText: "asnswer 4",
+      },
+    ],
+  },
+];
+
+const formatted = ref(questions);
+
+console.log(formatted.value);
+</script>
 
 <template>
   <span>QCM components</span>
+  <section class>
+    <form v-for="question in formatted" :key="question.id">
+      <span>{{ question.questionText }}</span>
+    </form>
+  </section>
 </template>
 
 <style></style>
