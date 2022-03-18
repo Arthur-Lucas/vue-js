2117ade4bbdaf5468b3a29fa526ba28efe60b06f
Kaowarstail <victor.dane@edu.devinci.fr>
2022-03-18 14:25:27 +0100

    clean App.vue + Hello world

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import Chrono from './components/Chrono.vue'
 </script>
 
 <template>
-  <Chrono></Chrono>
 </template>
 
 <style>
